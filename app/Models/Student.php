c3883210a8c8b086de32b0088604e5f973bf20ec
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-01-14 18:31:56 +0700

    change table

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $fillable = [
-        'nbi','name','place_of_birth','date_of_birth','phone','address','photo','latitude','longitude'
+        'nbi','name','place_of_birth','date_of_birth','phone','address',
+        'faculty','major','gender','hoby','nationality',
+        'photo','latitude','longitude'
     ];
 }
